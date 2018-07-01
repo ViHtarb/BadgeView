695dacedabd935605daf79c7616d42e1b1fdbb5a
Viнt@rь <vihtarb@gmail.com>
2018-07-01 23:53:04 +0300

    Sample app changes

--- app/src/main/java/com/badgeview/sample/MainActivity.java
+++ app/src/main/java/com/badgeview/sample/MainActivity.java
@@ -1,27 +1,28 @@
 package com.badgeview.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-import com.badgeview.BadgeActionView;
+import com.badgeview.BadgeActionMenuItemView;
 import com.badgeview.BadgeView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BadgeActionView mBadgeActionView;
+    private BadgeActionMenuItemView mBadgeActionView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final BadgeView badgeView = (BadgeView) findViewById(R.id.test_badge_view);
-        final TextView textView = (TextView) findViewById(R.id.test_text_view);
+        final BadgeView badgeView = findViewById(R.id.test_badge_view);
+        final TextView textView = findViewById(R.id.test_text_view);
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,16 +35,9 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
 
-        MenuItem menuItem = menu.findItem(R.id.action_test2);
-        mBadgeActionView = (BadgeActionView) menuItem.getActionView();
-        mBadgeActionView.setOnMenuItemClick(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                return onOptionsItemSelected(item);
-            }
-        });
-        mBadgeActionView.setMenuItem(menuItem);
-        mBadgeActionView.show();
+        MenuItem badgeMenuItem = menu.findItem(R.id.action_test2);
+        mBadgeActionView = (BadgeActionMenuItemView) badgeMenuItem.getActionView();
+        mBadgeActionView.showBadge();
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -51,10 +45,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_test2:
-                if (TextUtils.isEmpty(mBadgeActionView.getText())) {
-                    mBadgeActionView.setText("123");
+                if (TextUtils.isEmpty(mBadgeActionView.getBadgeText())) {
+                    mBadgeActionView.showBadge("123", false);
                 } else {
-                    mBadgeActionView.toggle();
+                    mBadgeActionView.toggleBadge();
                 }
                 break;
         }
